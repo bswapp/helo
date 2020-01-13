22ed9aa1ce474e875d737ba401a4ede5256de539
bswapp <brookeswapp@gmail.com>
2020-01-13 15:18:52 -0700

    ducks finished

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -6,10 +6,31 @@ const initalState = {
 const UPDATE_USER = "UPDATE_USER";
 const CLEAR_USER = "CLEAR_USER";
 
-export function updateUser(username, profile_pic) {}
+export function updateUser(username, profile_pic) {
+  console.log(username, profile_pic);
+
+  return {
+    type: UPDATE_USER,
+    payload: { username, profile_pic }
+  };
+}
 
 export function clearUser() {
   return { type: CLEAR_USER, payload: {} };
 }
 
-export default function reducer() {}
+export default function reducer(state = initalState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case UPDATE_USER:
+      return {
+        ...state,
+        username: payload.username,
+        profile_pic: payload.profile_pic
+      };
+    case CLEAR_USER:
+      return initalState;
+    default:
+      return state;
+  }
+}
